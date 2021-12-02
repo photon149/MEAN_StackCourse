ed05ca0795f88a294fec479486a4f1921deb10c9
photon149 <74582422+photon149@users.noreply.github.com>
2021-12-02 09:37:27 +0530

    Update mongoose.js

--- database/mongoose.js
+++ database/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb',{useNewUrlParser: true,useUnifiedTopology: true})
      .then(()=>{console.log('DB Conneced suceesfully')})
-     .catch((error)=>{console.log(error)});
+     .catch((error)=>{console.log("Error occured while db connection",error)});
 
 module.exports = mongoose;
